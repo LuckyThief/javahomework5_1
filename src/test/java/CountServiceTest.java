851e4f6b8d8b6ce2b148ee6886b1062b039bfd38
LuckyThief <luckythief13@gmail.com>
2023-02-20 11:47:24 +0400

    Parameterized test

--- src/test/java/CountServiceTest.java
+++ src/test/java/CountServiceTest.java
@@ -1,25 +1,16 @@
 import org.example.CountService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CountServiceTest {
 
-    @Test
-    public void CountingTheMonthsOfRest_1() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/count.csv")
+    public void CountingTheMonthsOfRest(int expected, int income, int expenses, int threshold) {
         CountService service = new CountService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void CountingTheMonthsOfRest_2() {
-        CountService service = new CountService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
